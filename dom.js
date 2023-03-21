267d52ca9b5effb4d122a034ae3f04fa9a0330d2
developer-abdulaziz <abdulazizwebdbdeveloper1996@gmail.com>
2023-03-21 22:39:02 +0600

    integrate javascript in a relation

---
+++ dom.js
@@ -0,0 +1,58 @@
+
+const liCollection = document.getElementsByTagName('li');
+/*
+for(let i = 0; i < liCollection.length; i++){
+    const index = i;
+    const element = liCollection[index];
+    console.log(index, element.innerText);
+}
+*/
+// for(const element of liCollection){
+//     console.log(element.innerText);
+// }
+
+
+
+
+
+//============================
+const allHeadings = document.getElementsByTagName('h1');
+/*
+for(let i = 0; i < allHeadings.length; i++){
+    const index = i;
+    const element = allHeadings[index];
+    console.log(index, element.innerText);
+}
+*/
+/*
+for(const element of allHeadings){
+    console.log(element.innerText);
+}
+*/
+
+//=================
+// const fruits = document.getElementById('fruits_title');
+// console.log(fruits.innerText='My Tour List');
+
+
+//===================
+const fruitsTitle = document.getElementById('fruits_title');
+
+fruitsTitle.innerText = 'Fruits changed by JS'
+
+//============
+const places = document.getElementsByClassName('important_places');
+
+/*
+for(let i = 0; i < places.length; i++){
+    const index = i;
+    const place = places[index];
+    console.log(place);
+}
+*/
+/*
+for(const place of places){
+    console.log(place.innerText);
+}
+*/
+
