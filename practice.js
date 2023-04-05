fe4723df30b6aa061b5ec68fa0cbb37abf7d3d76
developer-abdulaziz <abdulazizwebdbdeveloper1996@gmail.com>
2023-04-05 16:40:19 +0600

    pin matching file add

---
+++ practice.js
@@ -0,0 +1,9 @@
+const product = 5;
+ const price = "7"; 
+ const subTotal = product * price; 
+ const tax = subTotal/10; 
+ const total = subTotal + tax; 
+ console.log(total);
+ const category="pen"; 
+ const performer = category+"-teller";
+ console.log(performer);
