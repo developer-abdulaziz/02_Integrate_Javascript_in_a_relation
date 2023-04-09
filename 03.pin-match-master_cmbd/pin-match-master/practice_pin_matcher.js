0384a9c5f4c213f313a3a8213c63124d1a81c796
developer-abdulaziz <abdulazizwebdbdeveloper1996@gmail.com>
2023-04-09 11:38:42 +0600

    pin matcher

--- 03.pin-match-master_cmbd/pin-match-master/practice_pin_matcher.js
+++ 03.pin-match-master_cmbd/pin-match-master/practice_pin_matcher.js
@@ -1,46 +1,10 @@
-// Task 01 - Notification diasabled
 document.querySelector('.wrong-pin').style.display = 'none';
 document.querySelector('.correct-pin').style.display = 'none';
 
-// Task 02 - Generate Pin (4 digit pin)
 function generatePin(){
-    const pin = Math.floor(Math.random()*(9000) + 1000);
-    const generatePinField = document.querySelector('.generatedPin');
-    generatePinField.style.textAlign = 'center';
-    generatePinField.value = pin;
-
+    const pin = Math.floor(Math.random()*(9000+1000));
+    const generatedPin = document.querySelector('.generatedPin');
+    generatedPin.style.textAlign = 'center'; 
+    generatedPin.value = pin;
 }
-
-// Task 02 - (Input number populate on show value input) 
-// populate ortho holo dekhano
-function showInput(number){
-    const showInput = document.querySelector(".showValue");
-    showInput.value = showInput.value + number;
-    showInput.style.textAlign ="center"
-}
-
-// Task 04 - (Clear all input field) using C on calculate
-function clearInput(){
-    document.querySelector(".showValue").value = '';
-}
-
-
-
-// Task 05 - (Remove element one by one using "<")
-function removeElement(){
-    const currentValue = document.querySelector(".showValue").value;
-    const newCurrentValue = currentValue.substring(0, currentValue.length-1);
-    document.querySelector(".showValue").value = newCurrentValue;
-
-}
-
-
-/*
-// substring meaning
-let text = 'Hello world';
-let result = text.substring(0, 5);
-console.log(result);
-*/
-
-
 
--- 03.pin-match-master_cmbd/pin-match-master/solution.js
+++ 03.pin-match-master_cmbd/pin-match-master/solution.js
@@ -1,83 +1,94 @@
- //Notify None 
-
-        document.querySelector(".notify-section").style.display = "none";
-
-        //Generating Pin
-        
-        function generatePin(){
-            document.querySelector(".generatedPin").value = Math.floor(Math.random()*(9000) + 1000);
-            document.querySelector(".generatedPin").style.textAlign ="center"
-            
-        }
-
-        //Showing input 
-
-        function inputValue(number){
-            const showInput = document.querySelector(".showValue")
-            showInput.value = showInput.value + number;
-            showInput.style.textAlign ="center"
-
-        }
-
-        //Remove element
-
-        function removeElement(){
-            
-           const currentValue =  document.querySelector(".showValue").value
-           const newCurrentValue = currentValue.substring(0, currentValue.length - 1)
-           document.querySelector(".showValue").value = newCurrentValue   
+// Task 01 - Notification diasabled
+document.querySelector('.wrong-pin').style.display = 'none';
+document.querySelector('.correct-pin').style.display = 'none';
 
 
-        }
 
-        //Check Pin
 
-        function checkPin(){
-                const newPin = document.querySelector(".showValue").value
+// Task 02 - Generate Pin (4 digit pin)
+function generatePin(){
+    const pin = Math.floor(Math.random()*(9000) + 1000);
+    const generatePinField = document.querySelector('.generatedPin');
+    generatePinField.style.textAlign = 'center';
+    generatePinField.value = pin;
 
-                const generatedPin = document.querySelector(".generatedPin").value
+}
 
-                if(newPin == generatedPin){
-                   document.querySelector(".notify-section").style.display="block"
-                   document.querySelector(".correct-pin").style.display= "block"
-                   document.querySelector(".wrong-pin").style.display= "none"
-                   document.querySelector(".action-left").style.display ="none"
-                   document.querySelector(".submit-btn").disabled = true;
-                   document.querySelector(".generate-btn").disabled = true;
-                   document.querySelector(".submit-btn").style.backgroundColor = 'green';
-                   document.querySelector(".generate-btn").style.backgroundColor = 'green';
-                 
-                   
+// Task 02 - (Input number populate on show value input) 
+// populate ortho holo dekhano
+function showInput(number){
+    const showInput = document.querySelector(".showValue");
+    showInput.value = showInput.value + number;
+    showInput.style.textAlign ="center"
+}
 
-                }
+// Task 04 - (Clear all input field) using C on calculate
+function clearInput(){
+    document.querySelector(".showValue").value = '';
+}
 
-                else{
 
-                    document.querySelector(".notify-section").style.display="block"
-                    document.querySelector(".wrong-pin").style.display ="block";
-                    document.querySelector(".correct-pin").style.display ="none";
-                    document.querySelector(".action-left").style.display ="block";
 
-                    const tryLeft = document.querySelector("#tryLeft").innerText
-                    let newTryLeft = parseInt(tryLeft);
-                    if(newTryLeft > 0){
-                        newTryLeft = newTryLeft - 1 ;
-                    }
-                    document.querySelector("#tryLeft").innerText = newTryLeft;  
-                    
-                    if(newTryLeft === 0){
-                        document.querySelector(".submit-btn").disabled = true;
-                        document.querySelector(".submit-btn").style.backgroundColor = 'red';
-                        document.querySelector(".generate-btn").style.backgroundColor = 'red';
-                    }
-                }
-                
-                document.querySelector(".showValue").value = "";
-            
-                
-                
-        }
+// Task 05 - (Remove element one by one using "<")
+function removeElement(){
+    const currentValue = document.querySelector(".showValue").value;
+    const newCurrentValue = currentValue.substring(0, currentValue.length-1);
+    document.querySelector(".showValue").value = newCurrentValue;
 
-        function clearValue() {
-             document.querySelector(".showValue").value = null;
-        }
+}
+
+
+/*
+// substring meaning
+let text = 'Hello world';
+let result = text.substring(0, 5);
+console.log(result);
+*/
+
+// Task 06 - (check pin if the pin is matched or not)
+function checkPin(){
+    const generatedPin = document.querySelector('.generatedPin').value;
+    const newPin = document.querySelector('.showValue').value;
+    // console.log(generatedPin, newPin);
+
+    if(generatedPin == newPin){
+        document.querySelector('.correct-pin').style.display = 'block';
+        document.querySelector('.wrong-pin').style.display = 'none';
+
+        document.querySelector('.generate-btn').style.backgroundColor = 'green';
+        document.querySelector('.submit-btn').style.backgroundColor = 'green';
+        document.querySelector('.submit-btn').disabled = true;
+        document.querySelector('.generate-btn').disabled = true;
+
+    }
+    else{
+        document.querySelector('.correct-pin').style.display = 'none';
+        document.querySelector('.wrong-pin').style.display = 'block';
+
+        document.querySelector('.generate-btn').style.backgroundColor = 'red';
+        document.querySelector('.submit-btn').style.backgroundColor = 'red';
+        document.querySelector('.submit-btn').disabled = false;
+        document.querySelector('.generate-btn').disabled = true;
+
+
+
+       let tryLeft = document.getElementById('tryLeft').innerText;
+        // console.log(typeof tryLeft);
+        let newTryLeft = parseInt(tryLeft);
+         if(newTryLeft > 0){
+            newTryLeft = newTryLeft - 1;
+         }
+         document.getElementById('tryLeft').innerText = newTryLeft;
+         document.querySelector('.showValue').value = '';
+
+         if(newTryLeft === 0){
+        document.querySelector('.submit-btn').disabled = true;
+        document.querySelector('.submit-btn').style.backgroundColor = 'black';
+        document.querySelector('.submit-btn').innerText = 'PIN LOCKED!'
+
+
+         }
+    }
+}
+
+
